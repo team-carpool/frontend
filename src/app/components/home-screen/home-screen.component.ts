ba03a8d1aa1b3f0c53e01ab692828c14baa96657
Ayush <ayushkumar1610@outlook.com>
2022-11-04 09:03:13 +0530

    call route from home

--- src/app/components/home-screen/home-screen.component.ts
+++ src/app/components/home-screen/home-screen.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MapComponent } from '../map/map.component';
 
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.component.html',
   styleUrls: ['./home-screen.component.css']
 })
-export class HomeScreenComponent implements OnInit {
+export class HomeScreenComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(MapComponent) mapComp: MapComponent;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+      
+  }
+
+  public route(){
+    this.mapComp.getRoute();
   }
 
 }

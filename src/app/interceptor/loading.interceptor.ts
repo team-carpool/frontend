c6677fae889fa75635dd87be2125027df6fda165
Ayush <ayushkumar1610@outlook.com>
2023-02-26 01:14:44 +0530

    noLoaderUrl added in interceptor

--- src/app/interceptor/loading.interceptor.ts
+++ src/app/interceptor/loading.interceptor.ts
@@ -13,19 +13,28 @@ import { LoaderService } from '../services/load/loader.service';
 export class LoadingInterceptor implements HttpInterceptor {
 
   private totalRequests = 0;
+  private noLoaderUrl: Array<String> = [
+    "https://nominatim.openstreetmap.org/search",
+    "http://localhost:8080/travel/currentloc",
+  ]
+
 
   constructor(
     private loadingService: LoaderService
   ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log('caught')
-    this.totalRequests++;
-    this.loadingService.setLoading(true);
+    // console.log('caught '+request.url)
+    const url = request.url.split("?")[0];
+    
+    if(!this.noLoaderUrl.includes(url)){
+      this.totalRequests++;
+      this.loadingService.setLoading(true);
+    }
     return next.handle(request).pipe(
       finalize(() => {
         this.totalRequests--;
-        if (this.totalRequests == 0) {
+        if (this.totalRequests <= 0) {
           this.loadingService.setLoading(false);
         }
       })
